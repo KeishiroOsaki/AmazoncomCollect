47bab0c4b4fa4bc9863f512f69bb07968790be11
KeishiroOsaki <startipend2008@gmail.com>
2015-09-12 13:15:40 +0900

    GUI設計完了

--- src/InfoCollect.java
+++ src/InfoCollect.java
@@ -1,11 +1,22 @@
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JLabel;
+import javax.swing.JProgressBar;
+import javax.swing.JList;
 
 
 public class InfoCollect {
 
 	private JFrame frame;
+	private JButton btnStart;
+	private JButton btnPause;
+	private JLabel lblState;
+	private JProgressBar bar;
+	private JList listProcess;
 
 	/**
 	 * Launch the application.
@@ -35,8 +46,33 @@ public class InfoCollect {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setResizable(false);
+		frame.setBounds(100, 100, 545, 353);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		btnStart = new JButton("開始");
+		btnStart.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnStart.setBounds(29, 19, 117, 29);
+		frame.getContentPane().add(btnStart);
+		
+		btnPause = new JButton("中断");
+		btnPause.setBounds(158, 19, 117, 29);
+		frame.getContentPane().add(btnPause);
+		
+		lblState = new JLabel("状況");
+		lblState.setBounds(313, 24, 210, 16);
+		frame.getContentPane().add(lblState);
+		
+		bar = new JProgressBar();
+		bar.setBounds(29, 53, 494, 29);
+		frame.getContentPane().add(bar);
+		
+		listProcess = new JList();
+		listProcess.setBounds(29, 94, 494, 214);
+		frame.getContentPane().add(listProcess);
 	}
-
 }
